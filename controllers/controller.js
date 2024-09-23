319e8ba970de197a5925e5eb7d83d382aebad64e
Sherzik <sherzik99@gmail.com>
2024-09-23 16:44:56 +0500

    finished Auth Info by card project

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,52 +1,87 @@
 const hdb = require("../config/hdb");
+const oracledb = require('oracledb');
 
-async function getProcode(req, res) {
+async function getProcode(req, res, next) {
   let connection;
 
   const { card_num, startDate, endDate } = req.body;
-
   try {
-    console.log(card_num);
     connection = await hdb.getConnection();
     const query = `
       SELECT 
-        LTIMESTAMP, 
+        PRIMECARDSERNO as CardSerno,
+        ROWNUM as RowNumber,
+        I002_NUMBER as CardNumber,
+        I038_AUTH_ID as AuthID,
+        serno as AuthSerno,
+        LTIMESTAMP as AuthDateTime,
+        I004_AMT_TRXN as AuthAmount,
         OTB_AMT_CARD, 
-        AMT_CARD, 
-        I043A_MERCH_NAME, 
-        I043B_MERCH_CITY, 
-        I039_RSP_CD, 
-        I019_ACQ_COUNTRY, 
-        I000_MSG_TYPE, 
-        I003_PROC_CODE 
+        AMT_CARD as CardAmount, 
+        I049_CUR_TRXN as AuthCurrency,
+        CARDCURRENCY as CardCurrency,
+        I019_ACQ_COUNTRY as AuthCountry,
+        I032_ACQUIRER_ID as AcquirerID,
+        I043A_MERCH_NAME as MerchantName,
+        I018_MERCH_TYPE as MerchantType,
+        I042_MERCH_ID as MerchantID,
+        I043B_MERCH_CITY as MerchantCity, 
+        I022_POS_ENTRY as POSEntry,
+        SOURCE as AuthSource,
+        I039_RSP_CD as ResponseCode,
+        I037_RET_REF_NUM as RetrievalReferenceNumber,
+        REASONCODE as ReasonCode
       FROM 
         authorizations 
       WHERE 
-        I002_NUMBER = ${card_num}
+        I002_NUMBER=:card_num
       AND 
         LTIMESTAMP 
       BETWEEN 
-        TO_DATE( '${startDate}', 'DD-MM-YYYY') 
+        TO_DATE(:startDate, 'DD-MM-YYYY') 
       AND 
-        TO_DATE( '${endDate}', 'DD-MM-YYYY')
+        TO_DATE(:endDate, 'DD-MM-YYYY')
+      ORDER BY 
+        LTIMESTAMP
+      ASC
     `;
-    const result = await connection.execute(query);
 
+    const binds = {
+      card_num: {
+        type: oracledb.DB_TYPE_CHAR,
+        dir: oracledb.BIND_IN,
+        val: card_num,
+      },
+      startDate,
+      endDate,
+    };
+    const result = await connection.execute(query, binds, {});
     const columnNames = result.metaData.map(({ name }) => name);
-    const rows = result.rows.map((row) =>
-      Object.fromEntries(columnNames.map((name, index) => [name, row[index]]))
+    const rows = result.rows.map((row) => {
+      const objectRow = Object.fromEntries(
+        columnNames.map((name, index) => [
+          name,
+          typeof row[index] === "string" ? row[index].trim() : row[index],
+        ])
+      );
+
+      objectRow["ResponseDescription"] =
+        objectRow["REASONCODE"] === 0 ? "Approved" : "Declined";
+      return objectRow;
+    }
     );
 
     res.json({ rows });
   } catch (err) {
     console.error("Error executing query:", err);
-    res.status(500).send("Internal Server Error");
+    next(err);
   } finally {
     if (connection) {
       try {
         await connection.close();
       } catch (err) {
         console.error("Error closing connection:", err);
+        next(err);
       }
     }
   }
--- routers/router.js
+++ routers/router.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const { getProcode } = require("../controllers/controller");
+const errorHandler = require("../middlewares/errorHandler");
 
 const router = express.Router();
 
 router.post("/procode", async (req, res, next) => {
   try {
-    getProcode(req, res);
+    getProcode(req, res, next);
   } catch (err) {
     next(err);
   }
-});
+}, errorHandler);
 
 module.exports = router;
