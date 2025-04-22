cdef99a36b593a1651a6e0a0e2d7b7440233be86
Sherzik <sherzik99@gmail.com>
2025-04-22 16:15:13 +0500

    refactor: remove response codes and enhance operation types
    Remove the entire responseCodes.js module as it is no longer needed.  
    Refactor operationTypes.js by replacing the array with a detailed mapping  
    object that associates message types and processing codes to operation types.  
    Add a function to determine operation type by message and processing code,  
    including special handling for codes starting with '3' and '9'. This improves  
    clarity and extensibility for processing transaction types.

--- constants/operationTypes.js
+++ constants/operationTypes.js
@@ -1,37 +1,52 @@
-const OperationTypes = [
-  "012000",
-  "012000",
-  "002000",
-  "003000",
-  "010000",
-  "003000",
-  "011000",
-  "000000",
-  "010000",
-  "011000",
-  "014000",
-  "000000",
-  "000000",
-  "001000",
-  "010000",
-  "013000",
-  "000000",
-  "003030",
-  "000000",
-  "100000",
-  "170000",
-  "110000",
-  "100000",
-  "110000",
-  "170000",
-  "260000",
-  "200000",
-  "280000",
-  "203000",
-  "200000",
-  "200000",
-];
+const OPERATION_TYPES = {
+  "0410": {
+    "012000": "CR",
+    "003000": "CR",
+    "011000": "CR",
+    100000: "DR",
+    110000: "CR",
+    170000: "CR",
+  },
+  "0110": {
+    "012000": "DR",
+    "002000": "DR",
+    "010000": "DR",
+    "003000": "DR",
+    "000000": "DR",
+    "001000": "DR",
+    "013000": "DR",
+    "003030": "DR",
+    100000: "DR",
+    170000: "DR",
+    110000: "DR",
+    200000: "RC",
+    280000: "CR",
+    203000: "CR",
+    260000: "CR",
+  },
+  "0120": {
+    "000000": "DR",
+    "010000": "DR",
+    200000: "CR",
+  },
+  "0420": {
+    "000000": "CR",
+  },
+  "0430": {
+    "000000": "CR",
+  },
+};
 
-const getOperationTypeByProcCode = (procCode) => {
-    
-}
+function getOperationTypeByProcCode(msgType, procCode) {
+  const codeStr = String(procCode);
+  if (codeStr.startsWith("3")) {
+    return "BALANCE";
+  }
+
+  if (codeStr.startsWith("9")) {
+    return "PIN CHANGE";
+  }
+  return OPERATION_TYPES[msgType][procCode];
+};
+
+module.exports = { OPERATION_TYPES, getOperationTypeByProcCode };
